8fbf799899b42bbe20a3444c55cf31c5d37264da
Levy Araujo <levy4raujo@gmail.com>
2023-05-05 14:50:10 -0300

    [1º requisito concluído]
    Criando um leitor de arquivo com Fs e utilizando Get para listar o arquivo.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(express.json());
+
+const talker = path.resolve(__dirname, './talker.json');
+
+const readFile = async () => {
+  try {
+    const data = await fs.readFile(talker);
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(`Arquivo não pôde ser lido: ${error}`);
+  }
+};
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
@@ -13,4 +26,13 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', async (_req, res) => {
+  try {
+    const talkers = await readFile();
+    res.status(200).json(talkers);
+  } catch (err) {
+    res.status(200).send([]);
+  }
 });
